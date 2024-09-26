187fa75f84ae3709d89e8e559d6ed9411ddf056a
Gabriel <gujilderami@gmail.com>
2024-09-26 08:53:02 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(() => {
+
     const data = [
         {
             "course": "NSTP",
@@ -21,8 +22,17 @@ $(() => {
     ];
 
     data.forEach(item => {
-        console.log(item)
-        $("#table-with-js").append({})
+        console.log(item);
+        $("#table-with-js").append(
+            `<tr class="sem-header">
+            <td>${item["course"]}</td>
+            <td>${item["description"]}</td>
+            <td>${item["unit"]}</td>
+            <td>${item["grade"]}</td>
+            <td>${item["Remarks"]}</td>
+            <td>${item["Course"]}</td>
+            <td>${item["Term"]}</td>
+            </tr>`
+        )
     })
-    
 });
