2fba25da5c3a3dfd035b75f255d43184bba24c88
Gabriel <gujilderami@gmail.com>
2024-10-01 22:19:44 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,36 +2,42 @@ $(() => {
 
     const data = [
         {
-            "course": "NSTP",
-            "description": "Civic Welfare Training Service 1",
-            "unit": 3,
-            "grade": 1.3
+            "course": "Course",
+            "description": "Description",
+            "unit": "Unit",
+            "grade": "Grade",
+            "remarks": "Remarks",
+            "term": "Term"
         },
         {
-            "course": "NSTP",
+            "course": "NSTP01",
             "description": "Civic Welfare Training Service 1",
             "unit": 3,
-            "grade": 1.3
+            "grade": 1.90,
+            "remarks": "Passed",
+            "term": "First Semester A.Y. 2022-2023"
         },
         {
-            "course": "NSTP",
-            "description": "Civic Welfare Training Service 1",
+            "course": "SCITEC",
+            "description": "Science, Technology and Society",
             "unit": 3,
-            "grade": 1.3
+            "grade": 2.0,
+            "remarks": "Passed",
+            "term": "First Semester A.Y. 2022-2023"
         }
     ];
 
     data.forEach(item => {
         console.log(item);
         $("#table-with-js").append(
-            `<tr class="sem-header">
+            `<tr>
             <td>${item["course"]}</td>
             <td>${item["description"]}</td>
             <td>${item["unit"]}</td>
             <td>${item["grade"]}</td>
-            <td>${item["Remarks"]}</td>
-            <td>${item["Course"]}</td>
-            <td>${item["Term"]}</td>
+            <td>${item["remarks"]}</td>
+            <td>${item["course"]}</td>
+            <td>${item["term"]}</td>
             </tr>`
         )
     })
